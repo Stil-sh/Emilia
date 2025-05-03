5c08e75922e1864dc5356f9951f13ed3849ed9c1
Stil-sh <denios228s@gmail.com>
2025-05-03 20:13:21 +0300

    Update config.py

---
+++ anime_waifu_bot/config.py
@@ -0,0 +1,8 @@
+# Файл config.py
+import os
+
+# Токен вашего бота (возьмите у @BotFather в Telegram)
+BOT_TOKEN = "7954452949:AAFPjobmKF43QWu6oFC2szX_xTvoc9uClkk"
+
+# Прокси не используем (оставляем пустым)
+PROXY_URL = None
