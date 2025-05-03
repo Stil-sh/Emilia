e9a908f57eacf53dd5d577fdb3612e36d0ec0bef
Stil-sh <denios228s@gmail.com>
2025-05-03 23:19:58 +0300

    Update parser.py

--- anime_waifu_bot/parser.py
+++ anime_waifu_bot/parser.py
@@ -1,15 +1,13 @@
 import aiohttp
-import random
 
 async def get_danbooru_image(tags):
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(
                 "https://danbooru.donmai.us/posts/random.json",
-                params={"tags": tags}
+                params={"tags": f"{tags} rating:safe"}
             ) as resp:
                 data = await resp.json()
-                return data["file_url"]
-    except:
-        # Фолбэк если Danbooru недоступен
-        return "https://i.imgur.com/9pNffOY.jpg"
+                return data.get("file_url", "https://i.imgur.com/example.jpg")
+    except Exception:
+        return "https://i.imgur.com/example.jpg"  # Фолбэк изображение
