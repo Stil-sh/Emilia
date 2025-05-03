0870531793e1f23dace0f9d25c5f8d8c446f6659
Stil-sh <denios228s@gmail.com>
2025-05-03 23:04:47 +0300

    Update parser.py

--- anime_waifu_bot/parser.py
+++ anime_waifu_bot/parser.py
@@ -1,16 +1,10 @@
 import aiohttp
 import random
 
-async def get_random_danbooru_image():
-    async with aiohttp.ClientSession() as session:
-        try:
-            # Получаем случайный пост с рейтингом Safe
-            async with session.get(
-                "https://danbooru.donmai.us/posts/random.json?tags=rating:safe"
-            ) as response:
-                if response.status == 200:
-                    data = await response.json()
-                    return data["file_url"]
-                return "https://i.imgur.com/9pNffOY.jpg"  # Фолбэк-изображение
-        except Exception:
-            return "https://i.imgur.com/9pNffOY.jpg"  # Фолбэк при ошибке
+async def get_anime_image(category="", nsfw=False):
+    try:
+        if nsfw:
+            return "https://i.imgur.com/NSFW_EXAMPLE.jpg"
+        return f"https://i.imgur.com/{random.randint(100000, 999999)}.jpg"
+    except Exception:
+        return "https://i.imgur.com/DEFAULT_IMAGE.jpg"
