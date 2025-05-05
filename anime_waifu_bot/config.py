d106fb9111ce4627ff681f9782c56d948f16d2c5
Stil-sh <denios228s@gmail.com>
2025-05-05 18:43:49 +0300

    Delete anime_waifu_bot directory

--- anime_waifu_bot/config.py
+++
@@ -1,8 +0,0 @@
-# Файл config.py
-import os
-
-# Токен вашего бота (возьмите у @BotFather в Telegram)
-BOT_TOKEN = "7954452949:AAFPjobmKF43QWu6oFC2szX_xTvoc9uClkk"
-
-# Прокси не используем (оставляем пустым)
-PROXY_URL = None
